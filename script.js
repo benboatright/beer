bc919d839e693314e7b21695c492cb2021e66f21
benboatright <benhboatright@gmail.com>
2020-08-15 16:42:58 -0400

    clean up

--- script.js
+++ script.js
@@ -2,26 +2,25 @@
 let myLibrary=[];
 
 // function to create beer object
-function beer(name, brewery, state, type, rating) {
+function vacation(name, state, geography, rating) {
   this.name = name
-  this.brewery = brewery
   this.state = state
-  this.type = type
+  this.geography = geography
   this.rating = rating
 }
 
 //function to add beer
-function addBeerToLibrary(beer) {
-  myLibrary.push(beer)
+function addVacationToLibrary(vacation) {
+  myLibrary.push(vacation)
 }
 
 //add a couple beers to the array before rendering
-const quantumHop = new beer('Quantum Hop','Basic City','VA','DIPA','5 Stars')
-addBeerToLibrary(quantumHop)
-const hopDrone = new beer('Hop Drone', 'Pro Re Nata', 'VA', 'IPA','4 Stars')
-addBeerToLibrary(hopDrone)
-const iKnow = new beer('I Know, I Know', 'New Anthem', 'NC', 'IPA','4.5 Stars')
-addBeerToLibrary(iKnow)
+const kureBeach = new vacation('Kure Beach','VA','Beach','5 Stars')
+addVacationToLibrary(kureBeach)
+const newYork = new vacation('New York City', 'NY', 'Urban','4 Stars')
+addVacationToLibrary(newYork)
+const keystone = new vacation('Keystone', 'CO', 'Mountains','5 Stars')
+addVacationToLibrary(keystone)
 
 
 //render the library as card
@@ -33,19 +32,17 @@ function renderCard(myLibrary){
     var p2 = document.createElement('P')
     var p3 = document.createElement('P')
     var p4 = document.createElement('P')
-    var p5 = document.createElement('P')
     var button = document.createElement('BUTTON')
     button.setAttribute("id","deleteButton")
 
-    p1.innerText = 'Name: ' + myLibrary[i].name
-    p2.innerText = 'Brewery: ' + myLibrary[i].brewery
-    p3.innerText = 'State: ' + myLibrary[i].state
-    p4.innerText = 'Type: ' + myLibrary[i].type
-    p5.innerText = 'Rating: ' + myLibrary[i].rating
+    p1.innerText = 'City/Region: ' + myLibrary[i].name
+    p2.innerText = 'State: ' + myLibrary[i].state
+    p3.innerText = 'Geography: ' + myLibrary[i].geography
+    p4.innerText = 'Rating: ' + myLibrary[i].rating
     button.innerHTML = 'Delete'
     button.addEventListener("click",function(){div.remove()})
 
-    div.appendChild(p1).appendChild(p2).appendChild(p3).appendChild(p4).appendChild(p5)
+    div.appendChild(p1).appendChild(p2).appendChild(p3).appendChild(p4)
     div.append(button)
     div.setAttribute("id","card")
     document.getElementById("container").appendChild(div)
@@ -57,12 +54,11 @@ renderCard(myLibrary)
 // add data entered from ui
 function addData(){
   let new_name = document.getElementById('name').value;
-  let new_brewery = document.getElementById('brewery').value;
   let new_state = document.getElementById('state').value;
-  let new_type = document.getElementById('type').value;
+  let new_geography = document.getElementById('geography').value;
   let new_rating = document.getElementById('rating').value;
 
-  let brew = new beer(new_name,new_brewery,new_state,new_type,new_rating);
+  let vaca = new vacation(new_name,new_state,new_geography,new_rating);
   myLibrary.push(brew);
 
   var div = document.createElement('Div')
@@ -71,15 +67,13 @@ function addData(){
   var p2 = document.createElement('P')
   var p3 = document.createElement('P')
   var p4 = document.createElement('P')
-  var p5 = document.createElement('P')
   var button = document.createElement('BUTTON')
   button.setAttribute("id","deleteButton")
 
   p1.innerText = 'Name: ' + myLibrary[myLibrary.length -1].name
-  p2.innerText = 'Brewery: ' + myLibrary[myLibrary.length -1].brewery
-  p3.innerText = 'State: ' + myLibrary[myLibrary.length -1].state
-  p4.innerText = 'Type: ' + myLibrary[myLibrary.length -1].type
-  p5.innerText = 'Rating: ' + myLibrary[myLibrary.length -1].rating
+  p2.innerText = 'State: ' + myLibrary[myLibrary.length -1].state
+  p3.innerText = 'Geography: ' + myLibrary[myLibrary.length -1].geography
+  p4.innerText = 'Rating: ' + myLibrary[myLibrary.length -1].rating
   button.innerHTML = 'Delete'
   button.addEventListener("click",function(){div.remove()})
 
@@ -87,7 +81,6 @@ function addData(){
   div.appendChild(p2)
   div.appendChild(p3)
   div.appendChild(p4)
-  div.appendChild(p5)
   div.appendChild(button)
   document.getElementById("container").appendChild(div)
 
@@ -96,8 +89,7 @@ function addData(){
 // clear inputs in ui after submission
 function clearInputs(){
   document.getElementById('name').value = '';
-  document.getElementById('brewery').value='';
   document.getElementById('state').value='';
-  document.getElementById('type').value='';
+  document.getElementById('geography').value='';
   document.getElementById('rating').value='';
 }
